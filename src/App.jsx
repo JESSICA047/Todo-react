bd19719aeb371b436734b3d2c23f590ddf29b76d
Jessica Adzoyi <121783593+JESSICA047@users.noreply.github.com>
2025-02-10 12:41:27 +0000

    worked on new functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import Todo from "./components/Todo";
+import Form from "./components/form";
+import FilteredButton from "./components/FilteredButton";
 
 function App(props) {
+  const [tasks,setTasks] = useState(props.tasks);
+  function addTask(name) {
+    const newTask = {
+      "id":name,
+      "completed":false
+    };
+    setTasks([...tasks,newTasks])
+    alert(name)
+  }
   const taskList = props.tasks?.map((task) => (
     <Todo
       id={task.id}
@@ -13,43 +25,12 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask}/>
 
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show</span>
-          <span>All</span>
-          <span className="visually-hidden">Tasks</span>
-        </button>
-
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Active</span>
-          <span className="visually-hidden">Tasks</span>
-        </button>
-
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Completed</span>
-          <span className="visually-hidden">Tasks</span>
-        </button>
+        <FilteredButton/>
+        <FilteredButton/>
+        <FilteredButton/>
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
---
+++ src/components/FilteredButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FilteredButton() {
+  return (
+    <button type="button" className="btn toggle-btn" aria-pressed="true">
+      <span className="visually-hidden">Show</span>
+      <span>All</span>
+      <span className="visually-hidden">Tasks</span>
+    </button>
+  );
+}
+
+export default FilteredButton;
